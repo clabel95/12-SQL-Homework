d4fe7f01631141254537cecf1ba30980353e6f41
Clay Abel <clayton.abel.95@gmail.com>
2022-02-25 11:10:24 -0600

    Added some stuff to the seeds.sql but still need to figure out what i want the roles to be.

---
+++ seeds.sql
@@ -0,0 +1,15 @@
+USE cms_db;
+INSERT INTO department(name)
+VALUES ("Sales");
+INSERT INTO department(name)
+VALUES ("Engineering");
+INSERT INTO department(name)
+VALUES ("Finance");
+INSERT INTO department(name)
+VALUES ("Legal");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("", 50, 1), ("",142,2),("",451,3),("",999,4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Joe","Bob",1,3),("Mike","Joe",2,3),("Big","Boss",3);
